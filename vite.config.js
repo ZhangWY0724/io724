30fd371a937c49d67e92061bdf31153fffa50e79
张闻阳 <816653@crfsdi.com>
2025-11-21 18:36:53 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: "/io724/",  // 在此处加上base，其余部分不用改
+  base: "/",  // 在此处加上base，其余部分不用改
   plugins: [vue()],
   server: {
     port: 3000,
